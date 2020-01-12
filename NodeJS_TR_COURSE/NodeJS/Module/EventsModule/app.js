782f7de5378455cc1abcf7682501c5852c5668ed
mebaysan <menesbaysan@gmail.com>
2020-01-12 16:46:53 +0300

    eventm odül denemesi

--- NodeJS_TR_COURSE/NodeJS/Module/EventsModule/app.js
+++ NodeJS_TR_COURSE/NodeJS/Module/EventsModule/app.js
@@ -6,6 +6,10 @@ emitter.on('connection', () => {
     console.log('Bağlantı oluşturuldu!');
 });
 
+emitter.on('event_adi',(isim)=>{
+    console.log(`Merhaba ${isim}! event_adi isimli event tetiklendi ve çalıştı :)`);
+});
 
 // emit ediyoruz, eventi tetikliyoruz 
 emitter.emit('connection');
+emitter.emit('event_adi',('Enes'));
