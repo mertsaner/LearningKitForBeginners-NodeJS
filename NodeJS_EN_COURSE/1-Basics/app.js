76315971835107875b2c805f905ef1166a5c52a1
mebaysan <menesbaysan@gmail.com>
2020-01-22 20:33:35 +0300

    as

--- NodeJS_EN_COURSE/1-Basics/app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const routes = require('./routes');
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
-//console.log(req.url, req.method, req.headers);
-//console.log("We has a request!");
-console.log("Server started!");
-server.listen(3000);
